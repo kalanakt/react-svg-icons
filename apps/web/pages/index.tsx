15818b9af4c61da6b61c36d34e676d0ad7ad8b9e
kalanakt <e19198@eng.pdn.ac.lk>
2022-12-29 16:00:28 +0530

    remove until develop better `web` site and `doc` site

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,11 +1,9 @@
 import { Button } from "ui";
-import { HomeIcon } from "webicon"
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-      <HomeIcon />
       <Button />
     </div>
   );
