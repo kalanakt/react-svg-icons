fa21204738f857ac60c9c8c6d8a01c1fd9446624
kalanakt <e19198@eng.pdn.ac.lk>
2023-01-09 20:45:27 +0530

    web page mdi

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,163 @@
-import { Button } from "ui";
+import {
+  HomeIcon,
+  BookmarkIcon,
+  CategoryIcon,
+  ChatIcon,
+  CloseIcon,
+  BrowserIcon,
+  CheckIcon,
+  CloudIcon,
+  DatabaseIcon,
+  DocumentIcon,
+  DownIcon,
+  DownloadIcon,
+  CodeIcon,
+  GarbageIcon,
+  ForbiddenIcon,
+  GearIcon,
+  GridIcon,
+  MenuIcon,
+  HeartIcon,
+  HourglassIcon,
+  ImageIcon,
+  IdeaIcon,
+  LeftIcon,
+  LikeIcon,
+  LinkIcon,
+  ListIcon,
+  LocationIcon,
+  MapIcon,
+  MusicIcon,
+  MicrophoneIcon,
+  MegaphoneIcon,
+  MonitorIcon,
+  MobileIcon,
+  SitemapIcon,
+  SearchIcon,
+  RssIcon,
+  RightIcon,
+  PowerIcon,
+  ProcessIcon,
+  PinIcon,
+  PhotoIcon,
+  PaperIcon,
+  PerformanceIcon,
+  StarIcon,
+  StatsIcon,
+  TagIcon,
+  UserIcon,
+  UpIcon,
+  UploadIcon,
+  SvgIcon,
+  VideoIcon,
+  ArchiveIcon,
+  ArrowDownIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  ArrowUpIcon,
+  ArticleIcon,
+  AtIcon,
+  AwardIcon,
+  AttachmentIcon,
+  MoonIcon,
+  SunIcon,
+  BellIcon,
+  BellRingIcon,
+  BellSlashIcon,
+  BellOffIcon,
+  BellPlusIcon,
+  BellMinusIcon,
+} from '@react-svg-icons/web'
 
 export default function Web() {
   return (
-    <div>
-      <h1>Web</h1>
-      <Button />
+    <div className='box'>
+      <style tsx>{`
+         p {
+           color: blue;
+         }
+         div {
+           background: #37409;
+           padding: auto;
+           disply: flex;
+           flex-direction: row;
+           align-content: center;
+           align-items: center;
+         }
+         @media (max-width: 600px) {
+           div {
+             background: blue;
+           }
+         }
+       `}</style>
+      <HomeIcon />
+      <BookmarkIcon />
+      <CategoryIcon />
+      <ChatIcon />
+      <CloseIcon />
+      <BrowserIcon />
+      <CheckIcon />
+      <CloudIcon />
+      <DatabaseIcon />
+      <DocumentIcon />
+      <DownIcon />
+      <DownloadIcon />
+      <CodeIcon />
+      <GarbageIcon />
+      <ForbiddenIcon />
+      <GearIcon />
+      <GridIcon />
+      <MenuIcon />
+      <HeartIcon />
+      <HourglassIcon />
+      <ImageIcon />
+      <IdeaIcon />
+      <LeftIcon />
+      <LikeIcon />
+      <LinkIcon />
+      <ListIcon />
+      <LocationIcon />
+      <MapIcon />
+      <MusicIcon />
+      <MicrophoneIcon />
+      <MegaphoneIcon />
+      <MonitorIcon />
+      <MobileIcon />
+      <SitemapIcon />
+      <SearchIcon />
+      <RssIcon />
+      <RightIcon />
+      <PowerIcon />
+      <ProcessIcon />
+      <PinIcon />
+      <PhotoIcon />
+      <PaperIcon />
+      <PerformanceIcon />
+      <StarIcon />
+      <StatsIcon />
+      <TagIcon />
+      <UserIcon />
+      <UpIcon />
+      <UploadIcon />
+      <SvgIcon />
+      <VideoIcon />
+      <ArchiveIcon />
+      <ArrowDownIcon />
+      <ArrowLeftIcon />
+      <ArrowRightIcon />
+      <ArrowUpIcon />
+      <ArticleIcon />
+      <AtIcon />
+      <AwardIcon />
+      <AttachmentIcon />
+      <MoonIcon />
+      <SunIcon />
+      <BellIcon />
+      <BellRingIcon />
+      <BellSlashIcon />
+      <BellOffIcon />
+      <BellPlusIcon />
+      <BellMinusIcon />
     </div>
   );
 }
